3ecd3c02382c41908b58dea2ef83f4d8bcf0a3a0
Ken Le <leankhanh98@gmail.com>
2018-08-25 12:03:03 -0400

    fixing overlay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const main = document.getElementsByClassName('container');
 const overlays = document.getElementsByClassName('overlay');
 const card = document.getElementsByClassName('user-card');
-const randomUser = document.getElementsByClassName('user');
+const randomUser = document.querySelectorAll('.user');
 const exit = document.getElementsByClassName('exit-button');
 const next = document.getElementsByClassName('right-button');
 const previous = document.getElementsByClassName('left-button');
@@ -15,7 +15,6 @@ const previous = document.getElementsByClassName('left-button');
 fetch('https://randomuser.me/api/?results=12&nat=ca&inc=name,email,location,picture,cell,dob')
   .then(response => response.json())
   .then(data => generateUsers(data.results))
-  .then(data => addEvent())
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -25,54 +24,73 @@ function generateUsers(data) {
   for (let i = 0; i < data.length; i++) {
     const user = data[i];
     const html = `
-        <div class="user">
-          <span class="exit-button">&#935;</span>
-          <div class="overlay">
-            <span class="left-button">&lt;</span>
-            <div class="user-info">
-              <img class="user-image" src='${user.picture.large}' alt>
-              <h2 class="user-name">${user.name.first} ${user.name.last}</h2>
-              <p class="user-email">${user.email}</p>
-              <p class="user-city">${user.location.city}</p>
-              <hr>
-              <p class="user-phone">${user.cell}</p>
-              <p class="user-address">${user.location.street}, ${user.location.state} ${user.location.postcode}</p>
-              <p class="date-of-birth">DoB: ${user.dob.date.substring(5,7)}/${user.dob.date.substring(8,10)}/${user.dob.date.substring(0,4)}</p>
-            </div>
-            <span class="right-button">&gt;</span>
-          </div>
+      <div class="user-card">
+        <img class="user-image" src='${user.picture.large}' alt>
+        <div class="info">
+          <h2 class="user-name">${user.name.first} ${user.name.last}</h2>
+          <p class="user-email">${user.email}</p>
+          <p class="user-city">${user.location.city}</p>
         </div>
-        
-        <div class="user-card">
-          <img class="user-image" src='${user.picture.large}' alt>
-          <div class="info">
+      </div>
+    `;
+    main[0].innerHTML += html;
+
+    // Generating user modals when is clicked
+    for (let i = 0; i < randomUser.length; i++) {
+      randomUser[i].addEventListener('click', function() {
+        createOverlay[i];
+      });
+    }
+  }
+}
+
+function creatModals(data) {
+  for (let i = 0; i < data.length; i++) {
+    const user = data[i];
+    const html = `
+      <div class="user">
+        <span class="exit-button">&#935;</span>
+        <div class="overlay">
+          <span class="left-button">&lt;</span>
+          <div class="user-info">
+            <img class="user-image" src='${user.picture.large}' alt>
             <h2 class="user-name">${user.name.first} ${user.name.last}</h2>
             <p class="user-email">${user.email}</p>
             <p class="user-city">${user.location.city}</p>
+            <hr>
+            <p class="user-phone">${user.cell}</p>
+            <p class="user-address">${user.location.street}, ${user.location.state} ${user.location.postcode}</p>
+            <p class="date-of-birth">DoB: ${user.dob.date.substring(5,7)}/${user.dob.date.substring(8,10)}/${user.dob.date.substring(0,4)}</p>
           </div>
+          <span class="right-button">&gt;</span>
         </div>
-      `;
+      </div>
+    `;
     main[0].innerHTML += html;
   }
+}
+
+function creatOverlay(index) {
+
 }
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
 
-function addEvent() {
-  for (let i = 0; i < randomUser.length; i++) {
-    const container = main[0];
-    const userInfo = randomUser[i];
-    const close = exit[i];
-    const slideNext = next[i];
-    const slideBack = previous[i];
-    close.addEventListener('click', function() {
-      userInfo.style.display = "none";
-    })
-    container.addEventListener('click', function() {
-      userInfo.style.display = "block";
-    })
-
-  }
-}
+// function addEvent() {
+//   for (let i = 0; i < randomUser.length; i++) {
+//     const container = main[0];
+//     const userInfo = randomUser[i];
+//     const close = exit[i];
+//     const slideNext = next[i];
+//     const slideBack = previous[i];
+//     close.addEventListener('click', function() {
+//       userInfo.style.display = "none";
+//     })
+//     container.addEventListener('click', function() {
+//       userInfo.style.display = "block";
+//     })
+//
+//   }
+// }
