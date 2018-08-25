59c78db66d402b8d0ba8e2e976c73015e8dc91ab
Ken Le <leankhanh98@gmail.com>
2018-08-24 21:02:53 -0400

    fixing the user container

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const main = document.getElementsByClassName('container');
 const overlays = document.getElementsByClassName('overlay');
-const modals = document.getElementsByClassName('modals');
 const card = document.getElementsByClassName('user-card');
 const randomUser = document.getElementsByClassName('user');
 const exit = document.getElementsByClassName('exit-button');
+const next = document.getElementsByClassName('right-button');
+const previous = document.getElementsByClassName('left-button');
 
 
 
@@ -24,21 +25,24 @@ function generateUsers(data) {
   for (let i = 0; i < data.length; i++) {
     const user = data[i];
     const html = `
-      <div class="user">
-        <div class="overlay">
-          <div class="modals">
-            <h1 class="exit-button">&#935;</h1>
-            <img class="user-image" src='${user.picture.large}' alt>
-            <h2 class="user-name">${user.name.first} ${user.name.last}</h2>
-            <p class="user-email">${user.email}</p>
-            <p class="user-city">${user.location.city}</p>
-            <hr>
-            <p class="user-phone">${user.cell}</p>
-            <p class="user-address">${user.location.street}, ${user.location.state} ${user.location.postcode}</p>
-            <p class="date-of-birth">DoB: ${user.dob.date.substring(5,7)}/${user.dob.date.substring(8,10)}/${user.dob.date.substring(0,4)}</p>
+        <div class="user">
+          <span class="exit-button">&#935;</span>
+          <div class="overlay">
+            <span class="left-button">&lt;</span>
+            <div class="user-info">
+              <img class="user-image" src='${user.picture.large}' alt>
+              <h2 class="user-name">${user.name.first} ${user.name.last}</h2>
+              <p class="user-email">${user.email}</p>
+              <p class="user-city">${user.location.city}</p>
+              <hr>
+              <p class="user-phone">${user.cell}</p>
+              <p class="user-address">${user.location.street}, ${user.location.state} ${user.location.postcode}</p>
+              <p class="date-of-birth">DoB: ${user.dob.date.substring(5,7)}/${user.dob.date.substring(8,10)}/${user.dob.date.substring(0,4)}</p>
+            </div>
+            <span class="right-button">&gt;</span>
           </div>
         </div>
-
+        
         <div class="user-card">
           <img class="user-image" src='${user.picture.large}' alt>
           <div class="info">
@@ -47,7 +51,6 @@ function generateUsers(data) {
             <p class="user-city">${user.location.city}</p>
           </div>
         </div>
-      </div>
       `;
     main[0].innerHTML += html;
   }
@@ -59,14 +62,17 @@ function generateUsers(data) {
 
 function addEvent() {
   for (let i = 0; i < randomUser.length; i++) {
+    const container = main[0];
     const userInfo = randomUser[i];
-    const overlay = overlays[i];
     const close = exit[i];
-    userInfo.addEventListener('click', function() {
-      overlay.style.display = "block";
-    })
+    const slideNext = next[i];
+    const slideBack = previous[i];
     close.addEventListener('click', function() {
-      overlay.style.display = "none";
+      userInfo.style.display = "none";
     })
+    container.addEventListener('click', function() {
+      userInfo.style.display = "block";
+    })
+
   }
 }
